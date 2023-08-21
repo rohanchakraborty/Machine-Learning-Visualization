7cad9ea5d2b1f567b7935495b3e3efee66de0e7b
Rohan Chakraborty <65873610+rohanchakraborty@users.noreply.github.com>
2023-08-21 10:42:49 +0200

    Create tableGridSearch.py

---
+++ tableGridSearch.py
@@ -0,0 +1,29 @@
+def tableGridSearch(clf, all_columns=False, all_ranks=False, save=True):
+
+    # Convert the cross validated results in a DataFrame ordered by `rank_test_score` and `mean_fit_time`.
+    # As it is frequent to have more than one combination with the same max score,
+    # the one with the least mean fit time SHALL appear first.
+    cv_results = pd.DataFrame(clf.cv_results_).sort_values(by=['rank_test_score', 'mean_fit_time'])
+
+    # Reorder
+    columns = cv_results.columns.tolist()
+    # rank_test_score first, mean_test_score second and std_test_score third
+    columns = columns[-1:] + columns[-3:-1] + columns[:-3]
+    cv_results = cv_results[columns]
+
+    if save:
+        cv_results.to_csv('--'.join(cv_results['params'][0].keys()) + '.csv', index=True, index_label='Id')
+
+    # Unless all_columns are True, drop not wanted columns: params, std_* split*
+    if not all_columns:
+        cv_results.drop('params', axis='columns', inplace=True)
+        cv_results.drop(list(cv_results.filter(regex='^std_.*')), axis='columns', inplace=True)
+        cv_results.drop(list(cv_results.filter(regex='^split.*')), axis='columns', inplace=True)
+
+    # Unless all_ranks are True, filter out those rows which have rank equal to one
+    if not all_ranks:
+        cv_results = cv_results[cv_results['rank_test_score'] == 1]
+        cv_results.drop('rank_test_score', axis = 'columns', inplace = True)
+        cv_results = cv_results.style.hide_index()
+
+    display(cv_results)
